0d7c1c7956bb725f8e7a84223867b4a835886efc
Matt Gunter <mattg57@github.com>
2025-02-05 14:00:50 -0500

    update container to 22-slim

--- build/entrypoint.sh
+++ build/entrypoint.sh
@@ -6,6 +6,8 @@ set -e
 # Write environment variables to backend/.env
 echo "Entrypoint.sh Initializing backend environment variables..."
 env | grep -E '^PORT$|^PORT_|^GIT_|^BASE_' > /app/backend/.env
+set
+
 
 # Execute the passed command
 exec "$@"
